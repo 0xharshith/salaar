4b1b911401c799e7ca0739968892d90128ff160d
0xharshith <vharshit@gmail.com>
2023-12-09 23:47:28 +0530

    ads

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import Example from '@/components/sidebar'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+   <Example/>
+  )
+}
